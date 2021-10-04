515ad3b49447d8371c983538a216295f4d33b34f
CapnAvery <68193106+CapnAvery@users.noreply.github.com>
2021-10-04 19:53:25 +0200

    Added code to read file

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Armandt_sieberhagen_Quiz
 {
@@ -15,6 +16,26 @@ namespace Armandt_sieberhagen_Quiz
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String path = @"C:\Users\arman\source\repos\Armandt_sieberhagen_Quiz\bin\Debug\MemberNames.txt";
+
+            FileStream MyStream = new FileStream(path, FileMode.OpenOrCreate);
+
+            StreamReader reader = new StreamReader(MyStream);
+
+            String Line;
+
+            while((Line = reader.ReadLine()) != null)
+            {
+                listBox1.Items.Add(Line);
+            }
+            reader.Close();
+            MyStream.Close();
+            
         }
     }
 }
